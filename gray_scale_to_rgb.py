c5a5c3d2b06dd7a34745047a1cadbec6e2dc7488
ozy5 <umutozyurt1907@hotmail.com>
2023-08-21 17:56:44 +0300

    last

--- gray_scale_to_rgb.py
+++ gray_scale_to_rgb.py
@@ -1,8 +1,8 @@
 import numpy as np
+import cv2
 
-
-blue_mapping_start_point = 150
-red_mapping_start_point = 130
+blue_mapping_start_point = 200
+red_mapping_start_point = 175
 
 blue_mapping_slope = (255/(255-blue_mapping_start_point))
 red_mapping_slope = (255/(255-red_mapping_start_point))
@@ -10,5 +10,8 @@ red_mapping_slope = (255/(255-red_mapping_start_point))
 def gray_to_rgb_filter(img):
     return np.stack(((((img-blue_mapping_start_point)*(blue_mapping_slope)) * (img>blue_mapping_start_point)).astype(np.uint8), img, (((img-red_mapping_start_point)*(red_mapping_slope)) * (img>red_mapping_start_point)).astype(np.uint8)), axis=2)
 
+clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(4,4))
 
-
+def gray_to_rgb_with_clahe(img): #only to blue and red clannels
+    img_clahe = clahe.apply(img)
+    return np.stack(((((img_clahe-blue_mapping_start_point)*(blue_mapping_slope)) * (img_clahe>blue_mapping_start_point)).astype(np.uint8), img, (((img_clahe-red_mapping_start_point)*(red_mapping_slope)) * (img_clahe>red_mapping_start_point)).astype(np.uint8)), axis=2)
